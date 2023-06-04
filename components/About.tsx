76afe8df0b8f8f6ff93ff6c9f950626446360c0e
Arturo Alcazar <alcazararturo@hotmail.com>
2023-06-04 12:15:28 -0600

    Quito commit

--- components/About.tsx
+++ components/About.tsx
@@ -13,18 +13,18 @@ export const About = () => {
                 <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
                 <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10" viewBox="0 0 24 24">
-                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                    <circle cx="12" cy="7" r="4"></circle>
+                        <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                        <circle cx="12" cy="7" r="4"></circle>
                     </svg>
                 </div>
                 <div className="flex flex-col items-center text-center justify-center">
-                    <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">Angelica Alcazar Suarez</h2>
+                    <h2 className="font-medium title-font mt-4 text-gray-900 text-lg"><span>Angelica Alcazar Suarez</span></h2>
                     <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
-                    <p className="text-base">Raclette knausgaard hella meggs normcore williamsburg enamel pin sartorial venmo tbh hot chicken gentrify portland.</p>
+                    <p className="text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi vero est a, non deserunt facilis cupiditate similique et illo eaque fugiat in maxime, quaerat rerum consectetur fugit veritatis ipsa repudiandae.</p>
                 </div>
                 </div>
                 <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
-                <p className="leading-relaxed text-lg mb-4">Meggings portland fingerstache lyft, post-ironic fixie man bun banh mi umami everyday carry hexagon locavore direct trade art party. Locavore small batch listicle gastropub farm-to-table lumbersexual salvia messenger bag. Coloring book flannel truffaut craft beer drinking vinegar sartorial, disrupt fashion axe normcore meh butcher. Portland 90's scenester vexillologist forage post-ironic asymmetrical, chartreuse disrupt butcher paleo intelligentsia pabst before they sold out four loko. 3 wolf moon brooklyn.</p>
+                    <p className="leading-relaxed text-lg mb-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, aperiam doloribus fuga quaerat magnam porro quis amet expedita sit atque natus delectus. Iste exercitationem debitis magni unde sunt ducimus. Tempora?</p>
                 </div>
             </div>
             </div>
